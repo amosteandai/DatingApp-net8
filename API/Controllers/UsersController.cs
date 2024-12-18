a53bc8cb0d403da44132e76f3a39b0405a8dd57d
amoscherera <amos.cherera@wirk.co.za>
2024-12-18 14:08:02 +0200

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -14,7 +15,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
@@ -34,6 +35,25 @@ namespace API.Controllers
             }
 
             return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if(username == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            if(user == null){
+                if(username == null) return BadRequest("Could not find user");
+            }
+
+            mapper.Map(memberUpdateDto,user);
+
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
         }
     }
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,23 +1,35 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountService } from './account.service';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
-
   private http = inject(HttpClient);
   baseUral = environment.apiUrl;
-  private accountService =inject(AccountService);
+  members = signal<Member[]>([]);
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUral + 'users')
+    return this.http.get<Member[]>(this.baseUral + 'users').subscribe({
+      next: members => this.members.set(members)
+    })
   }
 
   getMember(username : string){
+    const member = this.members().find(x => x.userName === username);
+    if(member) return of(member)
+
     return this.http.get<Member>(this.baseUral + 'users/' + username)
+  }
+
+  updateMember(member : Member){
+    return this.http.put(this.baseUral + 'users', member).pipe(
+      tap(() =>{
+        this.members.update(members => members.map( m => m.userName === member.userName ? member : m))
+      })
+    )
   }
 }
